771e90e9613cdf63d942eef28add5d29f5ec5c35
kene_M <maduabumkene@gmail.com>
2024-04-09 19:53:14 -0500

    Added descriptions and given files.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+/**
+ * @author Kene, Skylar, Isaiah.
+ *
+ *
+ */
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // TODO create heap class??
+        // TODO create graph class??
+        // TODO implement Prim's Algorithm.
+
+        // TODO create graph using file.
+        // TODO output graph in adjacency list format (with weight).
+        // TODO output edges (with their weights) of the minimum spanning tree.
     }
 }
